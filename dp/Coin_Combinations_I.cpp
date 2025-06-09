63f5176e69c40235157e648edfbad8ac437f8da1
Ansh Gupta <anshgupta651@gmail.com>
2025-06-09 19:21:49 +0530

    Some changes in structure

--- dp/Coin_Combinations_I.cpp
+++ dp/Coin_Combinations_I.cpp
@@ -1,166 +1,30 @@
 // AG___
-// There is a one percent chance, and sometimes that chance is good enough.
-// But what matters is what you think about that one percent.
+// There is a one percent chance, and sometimes that chance is good enough. But what matters is what you think about that one percent.
 
-// Template Credits- Punpun018
-/****************************************************************************/
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long
-#define intmin LLONG_MIN
-#define intmax LLONG_MAX
-#define ll long long
-#define double long double
-#define uint unsigned long long
-#define inf 1000000000000000005
-const long long mod = 998244353;
-const ll N = 1e9 + 10;
-const int MAX = 2000005;
-#define yes cout << "YES" << "\n"
-#define no cout << "NO" << "\n"
-#define all(x) (x).begin(), (x).end()
-#define allr(x) (x).rbegin(), (x).rend()
 #define endl "\n"
-/**************************************************************************/
-ll gcd(ll a, ll b)
-{
-    if (b > a)
-    {
-        return gcd(b, a);
-    }
-    if (b == 0)
-    {
-        return a;
-    }
-    return gcd(b, a % b);
-}
-ll expo(ll a, ll b, ll mod)
-{
-    ll res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = (res * a) % mod;
-        a = (a * a) % mod;
-        b = b >> 1;
-    }
-    return res;
-}
-void extendgcd(ll a, ll b, ll *v)
-{
-    if (b == 0)
-    {
-        v[0] = 1;
-        v[1] = 0;
-        v[2] = a;
-        return;
-    }
-    extendgcd(b, a % b, v);
-    ll x = v[1];
-    v[1] = v[0] - v[1] * (a / b);
-    v[0] = x;
-    return;
-} // pass an arry of size1 3
-ll mminv(ll a, ll b)
-{
-    ll arr[3];
-    extendgcd(a, b, arr);
-    return arr[0];
-} // for non prime b
-ll mminvprime(ll a, ll b) { return expo(a, b - 2, b); }
-bool revsort(ll a, ll b) { return a > b; }
-ll combination(ll n, ll r, ll m, ll *fact, ll *ifact)
-{
-    ll val1 = fact[n];
-    ll val2 = ifact[n - r];
-    ll val3 = ifact[r];
-    return (((val1 * val2) % m) * val3) % m;
-}
-void google(int t) { cout << "Case #" << t << ": "; }
-vector<ll> sieve(int n)
-{
-    int *arr = new int[n + 1]();
-    vector<ll> vect;
-    for (int i = 2; i <= n; i++)
-        if (arr[i] == 0)
-        {
-            vect.push_back(i);
-            for (int j = 2 * i; j <= n; j += i)
-                arr[j] = 1;
-        }
-    return vect;
-}
-ll mod_add(ll a, ll b, ll m)
-{
-    a = a % m;
-    b = b % m;
-    return (((a + b) % m) + m) % m;
-}
-ll mod_mul(ll a, ll b, ll m)
-{
-    a = a % m;
-    b = b % m;
-    return (((a * b) % m) + m) % m;
-}
-ll mod_sub(ll a, ll b, ll m)
-{
-    a = a % m;
-    b = b % m;
-    return (((a - b) % m) + m) % m;
-}
-ll mod_div(ll a, ll b, ll m)
-{
-    a = a % m;
-    b = b % m;
-    return (mod_mul(a, mminvprime(b, m), m) + m) % m;
-} // only for prime m
-ll phin(ll n)
-{
-    ll number = n;
-    if (n % 2 == 0)
-    {
-        number /= 2;
-        while (n % 2 == 0)
-            n /= 2;
-    }
-    for (ll i = 3; i <= sqrt(n); i += 2)
-    {
-        if (n % i == 0)
-        {
-            while (n % i == 0)
-                n /= i;
-            number = (number / i * (i - 1));
-        }
-    }
-    if (n > 1)
-        number = (number / n * (n - 1));
-    return number;
-} // O(sqrt(N))
-/**********************************************************************************************/
-
-int count(int s, vector<int> &v, int n, int idx, vector<vector<int>> &dp)
-{
-    if (dp[s][idx] != -1)
-        return dp[s][idx];
-    if (s == 0)
-        return dp[s][idx] = 1;
-    if (s < 0 || idx == n)
-        return dp[s][idx] = 0;
-    int ans = count(s, v, n, idx + 1, dp);
-    if (s >= v[idx])
-        ans += count(s - v[idx], v, n, idx, dp);
-    return dp[s][idx] = ans;
-}
+#define mod 1000000007
 void sol()
 {
-    int n, s;
-    cin >> n >> s;
-    vector<int> v(n);
+    int n, x;
+    cin >> n >> x;
+    // Dp top down approch
+    vector<int> c(n);
     for (int i = 0; i < n; i++)
-        cin >> v[i];
-    vector<vector<int>> dp(s + 1, vector<int>(n + 1, -1));
-    int ans = count(s, v, n, 0, dp);
-    cout << ans << endl;
+        cin >> c[i];
+    vector<int> dp(x + 1, 0);
+    dp[0] = 1;
+    for (int i = 1; i <= x; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i - c[j] >= 0)
+                dp[i] = (dp[i] + dp[i - c[j]]) % mod;
+        }
+    }
+    cout << dp[x] << endl;
 }
 
 signed main()
@@ -169,7 +33,7 @@ signed main()
     cin.tie(NULL);
     int t;
     t = 1;
-    // cin >> t;
+    // cin>>t;
     while (t--)
     {
         sol();
